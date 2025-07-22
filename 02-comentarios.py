98c2e761cba83502b3c7262590eb422ee8b66dbc
Luis Chacha <luischacha82@gmail.com>
2025-07-22 09:26:39 -0500

    Comentarios de varias lineas

--- 02-comentarios.py
+++ 02-comentarios.py
@@ -1,4 +1,17 @@
 #Comentario de una linea
 
 #Imprime hola munda
-print("Hola Mundo...!!")
+print("Hola Mundo X...!!")
+
+"""
+Comentario de varias lineas
+usando comillas dobles
+"""
+
+
+'''
+Y por aca un cometario
+de multiples lineas pero usando 
+comillas sencillas
+
+'''
