2d9c34671edb57159c22bc6b56564ad231eac3cd
Luis Chacha <luischacha82@gmail.com>
2025-08-04 18:39:17 -0500

    Math operatioins

---
+++ 10-math-operations.py
@@ -0,0 +1,31 @@
+# suma
+print(2+2)  #4
+
+# resta
+print(4-2)  #2
+
+# multiplicacion
+print(4*2)  #8
+
+# division
+print(2/4)  #0.5
+
+# otras operaciones
+
+# potencia
+print(2 ** 3)   #8
+
+# division sin decimal
+print(5//4) #1
+
+# modulo(lo que sobra de la division: residuo)
+print (6%4) #2
+
+# redondear : round()
+print(round(3.1))  
+print(round(3.9))  
+
+# valor absoluto : abs()
+print(abs(20))  #20
+print(abs(-20))  #20
+print(abs(-20.34))  #20
