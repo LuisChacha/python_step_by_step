0682e34048eb9b69167473fc02d15c0031db7fd8
Luis Chacha <luischacha82@gmail.com>
2025-08-04 18:00:26 -0500

    String indexes

---
+++ 02-PrimerosPasos/07-string-indexes.py
@@ -0,0 +1,16 @@
+name = "Fernando"   # 4 letras 
+print(name)
+
+#print(len(name))
+
+#print(name[0])
+
+# Obtener la ultima letra
+print(name[-1])
+
+# [Start:Stop]
+print(name[0:])
+print(name[0:2])
+
+# [Start:Stop:StepOver]
+print(name[0:4:2])
