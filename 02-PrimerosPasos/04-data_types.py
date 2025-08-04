8768926f2789095a900c100a3a9d17ddeefc456f
Luis Chacha <luischacha82@gmail.com>
2025-08-03 22:42:09 -0500

    Tipos de datos

---
+++ 02-PrimerosPasos/04-data_types.py
@@ -0,0 +1,42 @@
+#Strings(cadenas, texto)
+saludo="Hola"
+first_name="Luis"
+last_name="Chacha"
+
+#int (enteros)
+numero = 2000 
+edad = 29
+gatos = 4
+
+#float: flotantes, decimales
+decimal = 20.42
+temparature=27.3
+
+#bool: booleanos, true o false
+booleano = True
+booleano_falso=False
+is_gamer = False
+
+# Datos especiales: no te asustes!!!
+#Listas: arrays
+lista=[1,2,3]
+lista_vocales=['a','e','i','o','u']
+
+#Tuplas
+tupla=(1,2,3,'q')
+
+#Set
+set_variable = {1,2,3,4}
+
+
+# Diccionario
+diccionario = {
+    'hi':"hola",
+    'bye':'adios'
+}
+
+informacion_personal = {
+    'name':'Luis',
+    'age':43,
+    'dogs':5
+}
