ff2396e398f83a2a928e26b35fad917908ffd7e3
Luis Chacha <luischacha82@gmail.com>
2025-08-04 17:47:40 -0500

    Strings formatting

---
+++ 02-PrimerosPasos/06-string-formatting.py
@@ -0,0 +1,9 @@
+first_name = "Luis"
+last_name = 'Chacha'
+age = 43
+# fullname = first_name + last_name
+fullname = f'{first_name} {last_name}'
+message = f'{fullname}, tienes {age} años'
+
+print(fullname)
+print(message)
