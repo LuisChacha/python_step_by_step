1c4f62dfb2412b520811e20d262584ff5f67b359
Luis Chacha <luischacha82@gmail.com>
2025-08-03 22:14:10 -0500

    Recomendaciones PEP8

--- 02-PrimerosPasos/03-variables.py
+++ 02-PrimerosPasos/03-variables.py
@@ -10,3 +10,12 @@ age=43
 salary=450.50
 print(age)
 print(salary)
+
+# Recomendaciones PEP8
+
+nombre_variables = 'Variables con snake_case'
+_variable_privada = 'Variable privada'
+PI = 3.1416
+print(nombre_variables)
+print(_variable_privada)
+print(PI)
