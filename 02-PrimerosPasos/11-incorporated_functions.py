45f9ec3fb9c14d08851b75c274250735aa3f20f6
Luis Chacha <luischacha82@gmail.com>
2025-08-04 18:49:29 -0500

    Funciones incorporadas

---
+++ 02-PrimerosPasos/11-incorporated_functions.py
@@ -0,0 +1,15 @@
+#  Funciones incorporadas - Funciones nativas
+print("Hola Luis")  # muestra un mensaje
+type(100)   # int
+str(100)    #'100'
+round(20.9)
+abs(-20)
+
+# Métodos - Tareas especificas
+message = "Algo que mostrar"
+message.upper() # ALGO QUE MOSTRAR
+
+numero = 100
+# numero.       Al usar un numero los metodos son especificos para números.
+print(message)
+print(message.replace('Algo', 'Nada'))
