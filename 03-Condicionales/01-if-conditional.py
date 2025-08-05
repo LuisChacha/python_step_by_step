efc7110976818d1c19b57ac7088e7febe66ea83a
Luis Chacha <luischacha82@gmail.com>
2025-08-05 17:56:58 -0500

    Condicionales if

--- 03-Condicionales/01-if-conditional.py
+++ 03-Condicionales/01-if-conditional.py
@@ -3,3 +3,9 @@ if is_old:
     print("Puedes manejar")
 else:
     print("Espera a cumplir la mayoria de edad")
+
+is_student = True
+if is_student:
+    print("Obten la licencia de estudiante")
+else:
+    print("Lo siento, necesitas comprar una licencia normal")
