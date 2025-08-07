0ff5b520b56b4ac448dcf1ff5f34d1415372d04d
Luis Chacha <luischacha82@gmail.com>
2025-08-06 23:38:05 -0500

    Operador ternario

--- 03-Condicionales/03-ternary-operator.py
+++ 03-Condicionales/03-ternary-operator.py
@@ -1,6 +1,13 @@
 # if-else en una sola linea
-is_student = False
+is_student = True
 if is_student:
     print("Licencia de estudiante")
 else:
     print("Licencia normal")
+
+# True if condicion else False
+
+get_license = "Licencia de estudiante" if is_student else "Licencia normal"
+print(get_license)
+
+
