96dfc18858cb4ae3a1427eeeac9133e11a051425
Luis Chacha <luischacha82@gmail.com>
2025-08-09 19:42:34 -0500

    Logical operator

---
+++ 03-Condicionales/06-logical-operator.py
@@ -0,0 +1,17 @@
+# Evaluar condiciones
+
+# and
+print(True and True)    #True
+print(True and False)   #False
+print(False and True)   #False
+print(False and False)  #False
+
+# or
+print(True or True)    #True
+print(True or False)   #True
+print(False or True)   #True
+print(False or False)  #False
+
+#not (negar)
+print(not True)
+print(not False)
