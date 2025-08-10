b2f3999042419ba633c9a46f707d382399d5a1a2
Luis Chacha <luischacha82@gmail.com>
2025-08-09 19:13:59 -0500

    Thuthy y falsey

--- 03-Condicionales/04-truthy-falsey.py
+++ 03-Condicionales/04-truthy-falsey.py
@@ -6,7 +6,16 @@ print(bool(1.1))
 print(bool(-1.1))
 print(bool(1j))
 print(bool("Hola"))
-print(bool([1,2,3]))
-
+print(bool([1,2,3]))    # lista/arreglo
+print(bool({1,2,3}))    # set
+print(bool((1,2,3)))    # tupla
 #Falsey (Falsos)
 print(bool(False))
+print(bool(0))
+print(bool(0.0))
+print(bool(0j))
+print(bool(""))
+print(bool([]))    # lista/arreglo
+print(bool({}))    # set
+print(bool(()))    # tupla
+print(bool(None))    # tupla
