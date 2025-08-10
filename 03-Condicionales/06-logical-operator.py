1845c0e19bac6fe977dbdf327ecf7cfd5a2e246f
Luis Chacha <luischacha82@gmail.com>
2025-08-09 20:48:07 -0500

    Logical operator

--- 03-Condicionales/06-logical-operator.py
+++ 03-Condicionales/06-logical-operator.py
@@ -15,3 +15,20 @@ print(False or False)  #False
 #not (negar)
 print(not True)
 print(not False)
+
+# and 
+age=25
+licensed=True
+if age >=18 and license:
+    print("Puedes conducir")
+
+# or
+is_student=False
+membership=True
+if is_student or membership:
+    print("Obtienes un descuento especial")
+
+# not
+is_admin=False
+if not is_admin:
+    print("Acceso denegado")
